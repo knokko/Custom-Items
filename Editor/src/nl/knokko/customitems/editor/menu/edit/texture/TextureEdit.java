18ecb311505b5b75b40345e2e907aee69a912085
knokko <knokogator@hotmail.com>
2018-10-03 20:46:17 +0200

    Forgot those files

--- Editor/src/nl/knokko/customitems/editor/menu/edit/texture/TextureEdit.java
+++ Editor/src/nl/knokko/customitems/editor/menu/edit/texture/TextureEdit.java
@@ -1,22 +1,38 @@
 package nl.knokko.customitems.editor.menu.edit.texture;
 
+import java.awt.Color;
+
 import nl.knokko.customitems.editor.menu.edit.EditMenu;
 import nl.knokko.customitems.editor.set.NamedImage;
+import nl.knokko.gui.color.GuiColor;
+import nl.knokko.gui.color.SimpleGuiColor;
 import nl.knokko.gui.component.menu.GuiMenu;
+import nl.knokko.gui.component.text.TextButton;
+import nl.knokko.gui.component.text.TextComponent;
+import nl.knokko.gui.util.TextBuilder.Properties;
 
 public class TextureEdit extends GuiMenu {
 	
 	protected final EditMenu menu;
 	protected final NamedImage previous;
+	protected final TextComponent errorComponent;
 
 	public TextureEdit(EditMenu menu, NamedImage previous) {
 		this.menu = menu;
 		this.previous = previous;
+		errorComponent = new TextComponent("", Properties.createLabel(Color.RED));
+	}
+	
+	@Override
+	public GuiColor getBackgroundColor() {
+		return SimpleGuiColor.GREEN;
 	}
 
 	@Override
 	protected void addComponents() {
-		// TODO Auto-generated method stub
-
+		addComponent(errorComponent, 0.1f, 0.9f, 0.9f, 1f);
+		addComponent(new TextButton("Cancel", Properties.createButton(new Color(200, 0, 0), new Color(50, 0, 0)), Properties.createButton(new Color(250, 0, 0), new Color(65, 0, 0)), () -> {
+			state.getWindow().setMainComponent(menu.getTextureOverview());
+		}), 0.1f, 0.7f, 0.25f, 0.8f);
 	}
 }
--- Editor/src/nl/knokko/customitems/editor/menu/edit/texture/TextureOverview.java
+++ Editor/src/nl/knokko/customitems/editor/menu/edit/texture/TextureOverview.java
@@ -1,19 +1,50 @@
 package nl.knokko.customitems.editor.menu.edit.texture;
 
+import java.awt.Color;
+
 import nl.knokko.customitems.editor.menu.edit.EditMenu;
 import nl.knokko.gui.component.menu.GuiMenu;
+import nl.knokko.gui.component.text.TextButton;
+import nl.knokko.gui.util.TextBuilder.Properties;
 
 public class TextureOverview extends GuiMenu {
 	
+	public static final Properties BUTTON_PROPERTIES = Properties.createButton(new Color(0, 100, 200), new Color(0, 25, 50));
+	public static final Properties HOVER_PROPERTIES = Properties.createButton(new Color(0, 125, 250), new Color(0, 40, 80));
+	
 	protected final EditMenu menu;
+	protected final TextureList textureList;
 
 	public TextureOverview(EditMenu menu) {
 		this.menu = menu;
+		textureList = new TextureList();
+	}
+	
+	@Override
+	public void init() {
+		if(didInit) textureList.refresh();
 	}
 
 	@Override
 	protected void addComponents() {
-		// TODO Auto-generated method stub
+		addComponent(textureList, 0.3f, 0f, 1f, 1f);
+		addComponent(new TextButton("Back", BUTTON_PROPERTIES, HOVER_PROPERTIES, () -> {
+			state.getWindow().setMainComponent(menu);
+		}), 0.05f, 0.7f, 0.2f, 0.8f);
+		addComponent(new TextButton("Create texture", BUTTON_PROPERTIES, HOVER_PROPERTIES, () -> {
+			state.getWindow().setMainComponent(new TextureEdit(menu, null));
+		}), 0.05f, 0.4f, 0.25f, 0.5f);
+	}
+	
+	private class TextureList extends GuiMenu {
 
+		@Override
+		protected void addComponents() {
+			refresh();
+		}
+		
+		private void refresh() {
+			
+		}
 	}
 }
