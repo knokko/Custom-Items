b75fab32845d3e3eece5824001fe0174fb086f7c
knokko <knokogator@hotmail.com>
2018-11-10 14:32:03 +0100

    Fix license headers

--- BasePlugin/src/nl/knokko/customitems/plugin/recipe/CustomRecipe.java
+++ BasePlugin/src/nl/knokko/customitems/plugin/recipe/CustomRecipe.java
@@ -1,15 +1,15 @@
-/*
+/*******************************************************************************
  * The MIT License
  *
- * Copyright 2018 knokko.
+ * Copyright (c) 2018 knokko
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
+ *  of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ *  
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
@@ -20,7 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- */
+ *******************************************************************************/
 package nl.knokko.customitems.plugin.recipe;
 
 import org.bukkit.inventory.ItemStack;
