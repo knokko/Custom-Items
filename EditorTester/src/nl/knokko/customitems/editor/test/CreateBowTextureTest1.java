8970022f4c1eda5f8a2dc415037262a31591443a
knokko <knokogator@hotmail.com>
2019-04-28 18:44:21 +0200

    Improve and clean the bow texture edit test

--- EditorTester/src/nl/knokko/customitems/editor/test/CreateBowTextureTest1.java
+++ EditorTester/src/nl/knokko/customitems/editor/test/CreateBowTextureTest1.java
@@ -1,17 +1,10 @@
 package nl.knokko.customitems.editor.test;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 
 import nl.knokko.gui.testing.GuiTestHelper;
 import nl.knokko.gui.testing.GuiTestProgram;
-import nl.knokko.gui.testing.TestException;
 
 /**
  * The first test for creating bows. Another less advanced test will be run after the item set has been
@@ -20,32 +13,6 @@ import nl.knokko.gui.testing.TestException;
  *
  */
 public class CreateBowTextureTest1 implements GuiTestProgram {
-	
-	private static final BufferedImage BAD_SIZED_IMAGE = new BufferedImage(16, 15, BufferedImage.TYPE_INT_ARGB);
-	private static final BufferedImage BAD_SIZED_IMAGE2 = new BufferedImage(30, 30, BufferedImage.TYPE_INT_ARGB);
-	private static final BufferedImage IMAGE1 = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
-	private static final BufferedImage IMAGE2 = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
-	private static final BufferedImage IMAGE3 = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
-	private static final BufferedImage IMAGE4 = new BufferedImage(128, 128, BufferedImage.TYPE_INT_ARGB);
-	
-	static {
-		Graphics2D g = IMAGE1.createGraphics();
-		g.setColor(Color.BLUE);
-		g.fillRect(0, 0, 32, 32);
-		g.dispose();
-		g = IMAGE2.createGraphics();
-		g.setColor(Color.green);
-		g.fillRect(0, 0, 16, 16);
-		g.dispose();
-		g = IMAGE3.createGraphics();
-		g.setColor(Color.RED);
-		g.fillRect(0, 0, 64, 64);
-		g.dispose();
-		g = IMAGE4.createGraphics();
-		g.setColor(Color.YELLOW);
-		g.fillRect(0, 0, 128, 128);
-		g.dispose();
-	}
 
 	@Override
 	public void test(GuiTestHelper test) {
@@ -57,17 +24,12 @@ public class CreateBowTextureTest1 implements GuiTestProgram {
 		test.click("Create");
 		test.assertComponentWithText("You need to give this bow a base texture.");
 		
-		try {
-			File folder = new File(".").getAbsoluteFile().getParentFile();
-			ImageIO.write(BAD_SIZED_IMAGE, "PNG", new File(folder + "/autotest0.png"));
-			ImageIO.write(BAD_SIZED_IMAGE2, "PNG", new File(folder + "/autotest00.png"));
-			ImageIO.write(IMAGE1, "PNG", new File(folder + "/autotest1.png"));
-			ImageIO.write(IMAGE2, "PNG", new File(folder + "/autotest2.png"));
-			ImageIO.write(IMAGE3, "PNG", new File(folder + "/autotest3.png"));
-			ImageIO.write(IMAGE4, "PNG", new File(folder + "/autotest4.png"));
-		} catch (IOException ioex) {
-			throw new TestException("Couldn't write necessary images for bow texture test 1:", ioex);
-		}
+		test.createImage("autotest0.png", 16, 15);
+		test.createImage("autotest00.png", 30, 30);
+		BufferedImage image1 = test.createImage("autotest1.png", 32, 32);
+		BufferedImage image2 = test.createImage("autotest2.png", 16, 16);
+		BufferedImage image3 = test.createImage("autotest3.png", 64, 64);
+		BufferedImage image4 = test.createImage("autotest4.png", 128, 128);
 		
 		test.clickNearest("Edit...", test.getComponentWithText("Base texture: "), 0, 4);
 		test.click("autotest0.png");
@@ -78,6 +40,7 @@ public class CreateBowTextureTest1 implements GuiTestProgram {
 		test.clickNearest("Edit...", "Base texture: ", 4);
 		test.click("autotest1.png");
 		test.click("Select");
+		test.assertNearestImage("Base texture: ", image1, 4);
 		test.click("Create");
 		test.assertComponentWithText("You can't leave the name empty.");
 		test.click("");
@@ -100,6 +63,7 @@ public class CreateBowTextureTest1 implements GuiTestProgram {
 		test.clickNearest("Edit...", "0.0", 4);
 		test.click("autotest1.png");
 		test.click("Select");
+		test.assertNearestImage("0.0", image1, 5);
 		test.click("Create");
 		test.assertComponentWithText("Pull 0.65 doesn't have a texture");
 		test.clickNearest("Edit...", "0.65", 4);
@@ -109,6 +73,7 @@ public class CreateBowTextureTest1 implements GuiTestProgram {
 		test.clickNearest("Edit...", "0.65", 4);
 		test.click("autotest2.png");
 		test.click("Select");
+		test.assertNearestImage("0.65", image2, 6);
 		test.click("0.65");
 		test.pressAndRelease(KeyEvent.VK_BACK_SPACE, 2);
 		test.type("45");
@@ -118,6 +83,7 @@ public class CreateBowTextureTest1 implements GuiTestProgram {
 		test.clickNearest("Edit...", "0.6", 4);
 		test.click("autotest3.png");
 		test.click("Select");
+		test.assertNearestImage("0.6", image3, 7);
 		test.click("Add pull");
 		test.click("0.3");
 		test.pressAndRelease(KeyEvent.VK_BACK_SPACE);
@@ -125,6 +91,7 @@ public class CreateBowTextureTest1 implements GuiTestProgram {
 		test.clickNearest("Edit...", "0.95", 5);
 		test.click("autotest4.png");
 		test.click("Select");
+		test.assertNearestImage("0.95", image4, 9);
 		test.click("Create");
 		test.assertComponentWithText("Ehm...");
 	}
