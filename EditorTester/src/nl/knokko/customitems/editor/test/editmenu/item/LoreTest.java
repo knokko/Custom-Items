d7571792395aa08cc35d920277ca4eb99e00b650
knokko <knokogator@hotmail.com>
2019-07-04 18:46:01 +0200

    Make a delay longer to prevent the tests from failing

--- EditorTester/src/nl/knokko/customitems/editor/test/editmenu/item/LoreTest.java
+++ EditorTester/src/nl/knokko/customitems/editor/test/editmenu/item/LoreTest.java
@@ -45,7 +45,7 @@ public class LoreTest {
 		test.type(lore1);
 		
 		// Appears necessary to delay briefly
-		test.delay(50);
+		test.delay(80);
 		test.click("Add line");
 		test.click("Text...");
 		test.backspace(7);
